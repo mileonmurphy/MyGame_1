a6e46407e9c718f314734c36dbbc28400b895a8b
Miguel Leon Murphy <mileonmurphy@gmail.com>
2016-04-18 14:51:11 -0400

    Commit - II 4/18
    Started work on Game Scene, in class

---
+++ My_Game_1/Assets/Scripts/PATTLE.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PATTLE : MonoBehaviour {
+
+
+	public Vector3 pattlePosition;
+	public Vector3 pattleVelocity;
+	public float pattleSpeed = 10f;
+
+	public bool touchBall;
+	public bool hazKittens;
+
+
+
+	// Use this for initialization
+	void Start () {
+		pattlePosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3(Input.GetAxis("Horizontal") * pattleSpeed, pattlePosition.y, -1) ;
+
+
+
+	}
+}
---
+++ My_Game_1/Assets/Scripts/loader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loader : MonoBehaviour {
+
+public void StartGame()
+	{
+		Application.LoadLevel("Game");
+	}
+}
