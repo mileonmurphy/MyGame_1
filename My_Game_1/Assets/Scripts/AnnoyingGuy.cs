2d6d5b3a51446546f3016730b2bf3e3f997bc184
Ben <bxc3201@rit.edu>
2016-04-27 14:27:29 -0400

    fix hammer

--- My_Game_1/Assets/Scripts/AnnoyingGuy.cs
+++ My_Game_1/Assets/Scripts/AnnoyingGuy.cs
@@ -1,34 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AnnoyingGuy : MonoBehaviour {
 
 	protected int state = 0;
-	protected float timer;
+	protected float actionTimer;
+	protected float animTimer;
 	protected Vector3 targetPos;
 	protected Vector3 startPos;
 	public brick brickPrefab;
 	protected GameObject brickToBuild;
 	float targetDist;
 	float speed = 5;
+	protected SpriteRenderer sr;
+	public Sprite[] pose;
+	public Sprite[] build;
+	public Sprite hitUp;
+	public Sprite hitDown;
+	public Sprite sit;
 
 	// Use this for initialization
 	void Start () {
-
+		sr = GetComponent<SpriteRenderer> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
+		actionTimer += Time.deltaTime;
+		animTimer += Time.deltaTime;
 		// do stuff based on state
 		switch (state) {
 		case 0:
 			break;	
 		case 1:
 			MovingToBrick ();
+			AnimateWalk();
 			break;
 		case 2:
 			BuildingBrick ();
+			AnimateBuild ();
 			break;
 		default:
 			break;
@@ -41,22 +52,23 @@ public class AnnoyingGuy : MonoBehaviour {
 		targetPos = brickToBuild.transform.position;
 		startPos = transform.position;
 		targetDist = (targetPos - startPos).magnitude;
-		timer = 0;
+		actionTimer = 0;
 	}
 
 	protected void MovingToBrick() {
 		// move towards brick
-		transform.position = Vector3.Lerp(startPos, targetPos, speed * timer / targetDist);
+		transform.position = Vector3.Lerp(startPos, targetPos, speed * actionTimer / targetDist);
 		// if you reach the brick, change state
-		if (speed * timer > targetDist) {
+		if (speed * actionTimer > targetDist) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, -2);
 			state = 2;
-			timer = 0;
+			actionTimer = 0;
 		}
 	}
 
 	protected void BuildingBrick() {
 		// timer until complete
-		if (timer > 2) {
+		if (actionTimer > 1.9) {
 			state = 0;
 			// move brick back in front of the camera
 			brickToBuild.transform.position = new Vector3(
@@ -65,5 +77,28 @@ public class AnnoyingGuy : MonoBehaviour {
 				-1);
 			//GameObject.Instantiate (brickPrefab,targetPos,Quaternion.identity);
 		}
+	}
+
+	// play the walk animation
+	protected void AnimateWalk() {
+		// every 1 seconds
+		if (animTimer > 1f) {
+			// max length
+			animTimer -= 1f;
+
+			// set sprite to next in sequence
+			sr.sprite = pose [(int)Mathf.Floor (Random.Range (0, pose.Length))];
+		}
+	}
+
+	protected void AnimateBuild() {
+		int speed = 2;
+
+		// max length
+		while (animTimer > build.Length/speed) {
+			animTimer -= build.Length/speed;
+		}
+		// set sprite to next in sequence
+		sr.sprite = build [(int)Mathf.Floor (animTimer*speed)];
 	}
 }
